7510e7c06d5a8812830197fd74cffbb502780f7f
amirjj <amir.jamshidijam@gmail.com>
2023-04-19 09:24:06 +0330

    Initial commit

---
+++ config.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+ROOT_PATH = Path.cwd()
+INPUT_DIR = ROOT_PATH.joinpath('input')
+OUTPUT_DIR = ROOT_PATH.joinpath('output')
---
+++ main.py
@@ -0,0 +1,8 @@
+from config import INPUT_DIR
+
+for file in INPUT_DIR.iterdir():
+	print(file)
+
+
+if __name__ == "__main__":
+	pass
